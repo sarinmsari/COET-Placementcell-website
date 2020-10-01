e63eff213a0dd906130ca30e5fdba317e0507fd8
fasalmbt <>
2020-10-02 01:42:46 +0530

    Added primary files🎉

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def home_page():
 	return render_template('index.html')
 
 #leaving rest of the functions for hacktoberfest participants
+#login,logout,notifications,scrapping to be added
 
 if __name__ == '__main__':
 	app.run(debug=True)
